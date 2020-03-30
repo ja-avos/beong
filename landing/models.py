fcc507c94f0f8ebe98c9863694d9bc32f9bb44b0
Javos <javelino2311@gmail.com>
2020-03-30 01:07:01 -0500

    Project files added

---
+++ landing/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Person(models.Model):
+    email = models.EmailField()
+    timeRegistered = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.email + ", time: " + str(self.timeRegistered)
---
+++ landing/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+from django.http import HttpResponseServerError
+from .models import Person
+import json
+
+def index(request):
+    return render(request, 'landing/index.html')
+
+@csrf_exempt
+def addPerson(request):
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        emailR = body["email"]
+        try:
+            person = Person(email=emailR)
+            person.save()
+        except Exception as e:
+            return HttpResponseServerError('No se pudo registrar :(' + str(e))
+        return HttpResponse("OK")
+    return HttpResponseBadRequest('Bad request')
+        
+
+# Create your views here.
