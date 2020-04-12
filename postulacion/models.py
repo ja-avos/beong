1913a598409c4786374afb46425e1a9f1eba5b9a
Carlos <ca.salazara@uniandes.edu.co>
2020-04-11 23:35:48 -0500

    Tolis, solo es rellenar eso. Voy a mimir. Mañana sigo temprano. <3
    Tolis, solo es rellenar eso. Voy a mimir. Mañana sigo temprano. <3

--- postulacion/models.py
+++ postulacion/models.py
@@ -5,7 +5,7 @@ from voluntariados.models import Voluntariado
 class Postulacion(models.Model):
     fecha= models.DateField()
     estado=models.CharField(max_length=20)
-    voluntariado=models.ForeignKey(Voluntariado, on_delete=None)
+    voluntariado=models.ForeignKey(Voluntariado, models.SET_NULL, null=True,)
 
 
 
