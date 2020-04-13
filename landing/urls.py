b5d91d73d666c93af5174ab20b8a3830f2e1d521
Carlos <ca.salazara@uniandes.edu.co>
2020-04-12 21:56:30 -0500

    Experiencias base

--- landing/urls.py
+++ landing/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('acerca', views.about, name='about'),
+    path('experiencias', views.experiences, name='experiences'),
     path('add', views.addPerson, name='addPerson')
 ]
