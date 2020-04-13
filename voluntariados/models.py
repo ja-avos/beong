114836a64357dda76b680d6ee4596b0d163b71a6
Violeta Rodriguez <vl.rodriguez10@uniandes.edu.co>
2020-04-13 18:09:21 -0500

    Llenando la DB 2

--- voluntariados/models.py
+++ voluntariados/models.py
@@ -6,13 +6,13 @@ class Voluntariado (models.Model):
     nombre = models.CharField(max_length=50)
     area=models.CharField(max_length=20)
     duracion=models.CharField(max_length=30)
-    descripcion=models.CharField(max_length=200)
-    lugar=models.CharField(max_length=25)
+    descripcion=models.CharField(max_length=300)
+    lugar=models.CharField(max_length=50)
     precio=models.FloatField()
     gustosRequeridos=models.ManyToManyField(Gusto)
     idiomasRequeridos=models.ManyToManyField(Idioma)
     calificaciones=models.ManyToManyField(Calificacion, blank=True)
-    imagen=models.CharField(max_length=150, blank=True)
+    imagen=models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return '%s' % (self.nombre)
