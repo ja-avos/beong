34eeaefd8617a09de45a1badfc0397cc476249c6
Javos <javelino2311@gmail.com>
2020-04-23 10:56:36 -0500

    Actualizado urls

--- voluntariados/urls.py
+++ voluntariados/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='voluntariados'),
     path('list', views.getVoluntariados, name='getVoluntariados'),
-    path('apply', views.apply_volunteer, name='apply')
+    path('apply', views.apply_volunteer, name='apply'),
+    path('create',views.createVoluntariado,name='createVoluntariado')
 ]
