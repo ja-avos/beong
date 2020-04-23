e1ccb833a21a573d3851ac675f9d99e668acf405
Javos <javelino2311@gmail.com>
2020-04-23 10:47:55 -0500

    Apply section created and filters

--- voluntariados/urls.py
+++ voluntariados/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='voluntariados'), #Falta hacerla
-    path('list', views.getVoluntariados, name='getVoluntariados')
+    path('', views.index, name='voluntariados'),
+    path('list', views.getVoluntariados, name='getVoluntariados'),
+    path('apply', views.apply_volunteer, name='apply')
 ]
